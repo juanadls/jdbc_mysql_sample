351beb2a28c7a7d9d21c9ac7ce682b0178015873
juanadls <eleo.dev09@gmail.com>
2025-01-01 15:54:56 -0300

    added formatting to file

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,24 @@ import java.util.Arrays;
 
 public class Main {
     private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+
     public static void main(String[] args) {
 
-String username = JOptionPane.showInputDialog(null, "Enter DB Username");
+        String username = JOptionPane.showInputDialog(null, "Enter DB Username");
 
-JPasswordField pf = new JPasswordField();
+        JPasswordField pf = new JPasswordField();
 
-int okcCxl = JOptionPane.showConfirmDialog(null, pf, "Enter db password", JOptionPane.OK_CANCEL_OPTION);
+        int okcCxl = JOptionPane.showConfirmDialog(null, pf, "Enter db password", JOptionPane.OK_CANCEL_OPTION);
 
-final char [] password = (okcCxl == JOptionPane.OK_OPTION)? pf.getPassword() : null;
+        final char[] password = (okcCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
 
 
-try (Connection connection = DriverManager.getConnection(CONN_STRING, username, String.valueOf(password))) {
-    System.out.println("Successfully connection");
-    Arrays.fill(password, '\0');
-} catch (SQLException e) {
-    throw  new RuntimeException(e);
+        try (Connection connection = DriverManager.getConnection(CONN_STRING, username, String.valueOf(password))) {
+            System.out.println("Successfully connection");
+            Arrays.fill(password, '\0');
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
 
-}
+        }
     }
 }
